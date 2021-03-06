80f3c7e36d465145e381acba4dab108e4f05a7d5
unknown <d.a.ramazanov@gmail.com>
2021-03-06 14:00:30 +0300

    Открытие меню версия 1

--- js/main.js
+++ js/main.js
@@ -6,7 +6,13 @@ $(document).ready(function() {
     prevArrow: '<button type="button" class="slider__btn slider__prev-btn"><img src="img/left-arrow.svg" alt="Назад" /></button>',
     nextArrow: '<button type="button" class="slider__btn slider__next-btn"><img src="img/right-arrow.svg" alt="Вперед" /></button>'
   });
+  //открытие закрытие меню
+  let menuBtn = document.querySelector(".menu-btn-js");
+  menuBtn.onclick= function () {
+    document.querySelector(".menu").classList.toggle("menu_active");
+  }
 
+  
   let textarea=document.querySelector(".js-textarea");
 
   textarea.addEventListener('keyup', function(){

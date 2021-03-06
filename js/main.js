2d42e7373e315440e17ca365ade09291f1119f18
unknown <d.a.ramazanov@gmail.com>
2021-03-06 12:34:10 +0300

    Сверстал футер

--- js/main.js
+++ js/main.js
@@ -1,8 +1,17 @@
 $(document).ready(function() {
+  //инициализация slick слайдера
   $(".js-slider").slick({
     dots: true,
     arrows: true,
     prevArrow: '<button type="button" class="slider__btn slider__prev-btn"><img src="img/left-arrow.svg" alt="Назад" /></button>',
     nextArrow: '<button type="button" class="slider__btn slider__next-btn"><img src="img/right-arrow.svg" alt="Вперед" /></button>'
+  });
+
+  let textarea=document.querySelector(".js-textarea");
+
+  textarea.addEventListener('keyup', function(){
+    if(this.scrollTop > 0){
+      this.style.height = this.scrollHeight + "px";
+    }
   });
 });

d48cae4d3b4dde6f2ce7e39806d868294269e6d1
DamirRam <59686045+DamirRam@users.noreply.github.com>
2021-03-07 21:23:44 +0300

    Update mail.php

--- mailer/mail.php
+++ mailer/mail.php
@@ -41,8 +41,8 @@ $mail->CharSet = 'utf-8';
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.yandex.ru';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'sendmailfromsite@yandex.ru';                 // Наш логин
-$mail->Password = 'ucgpwjsckdefidky';                           // Наш пароль от ящика
+$mail->Username = '';                 // Наш логин
+$mail->Password = '';                           // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
  

7d1ca0b562cca3f9919599484b15f161d86314e4
unknown <d.a.ramazanov@gmail.com>
2021-03-14 20:39:30 +0300

    Переделал косые линии, отредактировал код css и html, добавил faviconку

--- js/main.js
+++ js/main.js
@@ -14,7 +14,7 @@ $(document).ready(function() {
   $("a[href*='#']").on("click", function(e){
     var anchor = $(this);
     $('html, body').stop().animate({
-      scrollTop: $(anchor.attr('href')).offset().top-190
+      scrollTop: $(anchor.attr('href')).offset().top-150
     }, 777);
     e.preventDefault();
     return false;
